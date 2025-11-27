840f0d65b62da7cc18986fc48e87b015146d2b5b
tugbaberk <tugba.berk@d-teknoloji.com.tr>
2025-11-27 15:35:30 +0300

    Initial Commit

---
+++ atistirmaKutusu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  atistirmaKutusu
+//
+//  Created by tuğba berk on 27.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ atistirmaKutusu/atistirmaKutusuApp.swift
@@ -0,0 +1,17 @@
+//
+//  atistirmaKutusuApp.swift
+//  atistirmaKutusu
+//
+//  Created by tuğba berk on 27.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct atistirmaKutusuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
